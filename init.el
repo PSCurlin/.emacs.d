cc5f7b81b73386911474612d8095cb66a89407fd
Phaedra Curlin <phaedra.curlin@outlook.fr>
2025-06-09 16:30:03 -0600

    Update init.el

--- init.el
+++ init.el
@@ -25,3 +25,5 @@
 (define-key input-decode-map "\e[1;2A" [S-up])
 (define-key input-decode-map "\e[1;2C" [S-right])
 (define-key input-decode-map "\e[1;2D" [S-left])
+(define-key input-decode-map "\e[1;5C" [C-right])
+(define-key input-decode-map "\e[1;5D" [C-left])
